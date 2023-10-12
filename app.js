e9c8f4b310ef4751b988261c1222c07800a1f508
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-10-13 03:00:43 +0600

    Updated app.js settings

--- app.js
+++ app.js
@@ -34,6 +34,14 @@ app.use(
   }),
 );
 
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
+
+
 const userRouter = require('./src/routes/user.router');
 
 app.use('/', userRouter);
