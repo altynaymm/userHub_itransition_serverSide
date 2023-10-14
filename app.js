b9d724529a8f8ee80cfb876bc4f466bf0b7e2ce9
Altynay Muratkyzy <125205125+altynaymm@users.noreply.github.com>
2023-10-14 16:49:17 +0600

    app.js bug fixed

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ const morgan = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
+const app = express();
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const userRouter = require('./src/routes/user.router');
 
-const app = express();
+app.use(
+  cors({
+    origin: 'https://user-hub-itransition-client-side-dfov.vercel.app',
+    credentials: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    optionsSuccessStatus: 204,
+  }),
+);
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
@@ -30,28 +39,17 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 
 app.use(
-  cors({
-    origin: 'https://user-hub-itransition-client-side-dfov.vercel.app',
-    credentials: true,
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-    optionsSuccessStatus: 204,
-  }),
-);
-
-app.use(
   '/api',
   createProxyMiddleware({
-    target: ' https://userhub-itransition-db40c4fa7fa7.herokuapp.com/api',
+    target: 'https://userhub-itransition-db40c4fa7fa7.herokuapp.com',
     changeOrigin: true,
     pathRewrite: {
-      '^api': '',
+      '^/api': '',
     },
   }),
 );
 
-const userRouter = require('./src/routes/user.router');
-
-app.use('/api', userRouter);
+app.use('/', userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
